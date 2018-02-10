fdab7a1534a7e8009e45e55b028069a946aee05d
arham116 <arhamchowdhry@gmail.com>
2018-02-10 16:28:36 +0530

    confirm

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-// JavaScript source code
-var msg = require("./message.js");
-msg.msg1();
-msg.msg2();
+var fs = require("fs");
+var http = require("http");
+var url = require("url");
+
+http.createServer(function(req,res){
+  var addr = url.parse(req.url,true);
+  var q = addr.pathname;
+  var fileName = "." + q;
+  fs.readFile(fileName, function(err, data){
+    if(err){
+      res.writeHead(404,{'content-Type':'text/html'});
+      return res.end("404 not found")
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  })
+}).listen(84);
--- message.js
+++ message.js
@@ -1,9 +1,7 @@
-var msg1 = function () {
-    console.log("hi i am function 1")
+module.exports = function(firstName,lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.fullName = function(){
+        return this.firstName + " " + this.lastName
+    }
 }
-module.exports.msg1 = msg1;
-
-var msg2 = function () {
-    console.log("hi i am function 2")
-}
-module.exports.msg2 = msg2;
